8a60692ce159dfe1bf14a52cc8529e556fa6615b
Sean Marcoux <se_marc@berg.encs.concordia.ca>
2018-03-27 20:00:44 -0400

    Calculated the differences between the angles

--- readFile.m
+++ readFile.m
@@ -1,7 +1,8 @@
-function [Rho,Theta,Phi] = readFile(filename)
+function [thetaDiffs,phiDiffs] = readFile(filename)
     %READFILE Reads from filename and creates an array of data
     %   reads all lines of a file containing data in ses of 3 per line
     %   delimited by spaces
+    Time = {};
     Rho = {};
     Theta = {};
     Phi = {};
@@ -14,25 +15,51 @@ function [Rho,Theta,Phi] = readFile(filename)
     fclose(fileID);
     
     counter = 0;
+    %counter2 = 0;
     for index = 1:length(Data{1})
         if counter == 0
+            %counter2 = counter2 + 1;
+            %Time = [Time, num2str(counter2)];
             Rho = [Rho, Data{1}{index}];
             counter = counter + 1;
         elseif counter == 1
-            Theta = [Theta, Data{1}{index}];
+            degree = str2double(Data{1}(index));
+            disp(degree);
+            rad = degtorad(degree);
+            Theta = [Theta, rad];
             counter = counter + 1;
         else
-            Phi = [Phi, Data{1}{index}];
+            degree = str2double(Data{1}(index));
+            rad = degtorad(degree);
+            Phi = [Phi, rad];
             counter = 0;
         end
     end
     
+    thetaDiffs = {};
+    thetaDiffs = [thetaDiffs, 0];
+    phiDiffs = {};
+    phiDiffs = [phiDiffs, 0];
+    for index = 2:length(Theta)
+        disp(Theta{index});
+        currentThetaDiff = Theta{index} - Theta{index-1};
+        currentPhiDiff = Phi{index} - Phi{index-1};
+        thetaDiffs = [thetaDiffs, currentThetaDiff];
+        phiDiffs = [phiDiffs, currentPhiDiff];
+    end
     %disp(Rho)
     %disp(Theta)
     %disp(Phi)
-    matrix = [Rho Theta Phi];
+    VariableName = [Rho; Theta; Phi];
+    %disp(Rho);
+    %disp(Theta);
+    %matrix = {};
+    %matrix(1) = Rho;
+    %matrix(2) = Theta;
+    %matrix(3) = Phi;
     run('controlSystem');
-    set_param('controlSystem/From Workspace', 'VariableName', matrix);
+    %set_param('controlSystem/Step', 'Numerator', "1");
+    %set_param('controlSystem/From Workspace', 'VariableName', VariableName);
     %for index = 1:length(Theta)
     %    set_param('controlSystem/Constant','Value',Theta(index))
     %    pause(1)
